adf08ced165da57bd27979a032770a4a75bd5eeb
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-06-15 18:29:55 +1000

    form onchage event handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
 import { useState } from 'react';
 
 import './App.css';
+import Form from './components/Form';
 
 function App() {
-  return <></>;
+  return (
+    <div className="wrapper pt-5">
+      {/* <!-- title  --> */}
+      <div className="container">
+        <h1 className="text-center">Follow Time Table</h1>
+
+        {/* <!-- form  --> */}
+        <Form />
+
+        {/* <!-- tables --> */}
+        <div className="row mt-5">
+          <div className="col-md">
+            <h3 className="text-center">Entry List</h3>
+            <hr />
+            {/* <!-- Entry list table  --> */}
+            <table className="table table-striped table-hover border">
+              <tbody id="entryList"></tbody>
+            </table>
+          </div>
+          <div className="col-md">
+            <h3 className="text-center">Bad List</h3>
+            <hr />
+
+            {/* <!-- Bad List table --> */}
+            <table className="table table-striped table-hover border">
+              <tbody id="badList"></tbody>
+            </table>
+
+            <div className="alert alert-success">
+              You could have saved = <span id="savedHrsElm"></span> hr
+            </div>
+          </div>
+        </div>
+
+        <div className="alert alert-success">
+          The total hours allocated = <span id="ttlHrs">0</span> hrs
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+const Form = () => {
+  const [form, setForm] = useState({});
+
+  const handleOnChange = (e) => {
+    // console.log(e);
+    const { name, value } = e.target;
+    // console.log(name, value);
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  console.log(form);
+  return (
+    <form
+      className="border p-5 rounded shadow-lg mt-5"
+      action="javascript:void(0)"
+      onsubmit="handleSubmit(this)"
+    >
+      <div className="row g-2">
+        <div className="col-md-7">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Task"
+            aria-label="First name"
+            name="task"
+            id="task"
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="col-md-2">
+          <input
+            type="number"
+            className="form-control"
+            placeholder="40"
+            aria-label="Last name"
+            name="hr"
+            min="1"
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="col-md-3 d-grid">
+          <button className="btn btn-primary">Add New Task</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
